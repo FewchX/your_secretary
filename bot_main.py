2e9070f0f3ab791cdd7b7fb97939b164c99e61f6
Volodymyr Kolesnikov <volodymyr.kolesnikov51@gmail.com>
2025-07-24 21:19:03 +0000

    Fix bot stop message formatting and add newline in exception handling

--- bot_main.py
+++ bot_main.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        print("Bot Stopped. \nMessage - Папочка, я сделала что-то не так? Прости меня!")
+        print("\nBot Stopped. \nMessage - Папочка, я сделала что-то не так? Прости меня!")
     except Exception as e:
-        print(f"Обшибка{e}")
+        print(f"Обшибка {e}")
+    print("Bot Stopped. \nMessage - Папочка, я сделала что-то не так? Прости меня!")
